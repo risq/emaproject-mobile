a6df633cc5d3711031a27f71b39f726094527818
risq <valentin.ledrapier@gmail.com>
2015-03-31 14:53:28 +0200

    Minor js update

--- app/assets/javascripts/page1.js
+++ app/assets/javascripts/page1.js
@@ -20,41 +20,51 @@
 // button.addEventListener('click', alertAsyncMessage)
 
 
-var THREE = require('n3d-threejs')
+let THREE = require('n3d-threejs');
  
-var width = window.innerWidth
-var height = window.innerHeight
-var scene, camera, renderer, cube
+let width = window.innerWidth;
+let height = window.innerHeight;
+let scene, camera, renderer, cube;
  
-function init(){
-  scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000)
+function init () {
+
+	scene = new THREE.Scene()
+	camera = new THREE.PerspectiveCamera( 45, width/height, 0.1, 10000 );
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+	document.body.appendChild( renderer.domElement );
+
+	let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	camera.position.z = 5;
+
+}
+
+
  
-  renderer = new THREE.WebGLRenderer()
-  renderer.setSize(width, height)
-  document.body.appendChild(renderer.domElement)
- 
-  var geometry = new THREE.BoxGeometry(1, 1, 1)
-  var material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-  cube = new THREE.Mesh(geometry, material)
-  scene.add(cube)
- 
-  camera.position.z = 5
+function animate () {
+
+	requestAnimationFrame( animate );
+	update();
+	render();
+
 }
  
-function animate(){
-  requestAnimationFrame(animate)
-  update()
-  render()
-}
- 
-function update(){
-  cube.rotation.x += 0.1
-  cube.rotation.y += 0.1
+function update() {
+
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
+
 }
  
 function render(){
-  renderer.render(scene, camera)
+
+	renderer.render( scene, camera );
+	
 }
  
 init()
