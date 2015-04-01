fcb381837b9b66053738f94404c5ea100e0c0bf0
Julien Martins Da Costa <contact@julien-dacosta.fr>
2015-04-01 11:28:30 +0200

    fix compass source assets

--- gulpfile.js/config/compass.js
+++ gulpfile.js/config/compass.js
@@ -2,7 +2,7 @@ var config = require('./');
 
 module.exports = {
     autoprefixer: { browsers: ['last 2 version'] },
-    src: config.sourceAssets + '/stylesheets/**/*.{sass,scss}',
+    src: config.sourceAssets + '/stylesheets/**/main.{sass,scss}',
     dest: config.publicAssets + '/stylesheets',
     settings: {
         css: config.publicAssets + '/stylesheets',
