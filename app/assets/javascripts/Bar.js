7e41f4e7655be76c544736a4995a278f7bb383d7
risq <valentin.ledrapier@gmail.com>
2015-04-01 11:39:10 +0200

    Update three.js scene

--- app/assets/javascripts/Bar.js
+++ app/assets/javascripts/Bar.js
@@ -3,13 +3,29 @@ let THREE = require('n3d-threejs');
 
 class Bar {
 
-    constructor (size, pos, type) {
+    constructor (size, pos, type, opacity) {
 
+    	opacity = opacity || 1;
     	this.type = type;
 
-    	let material =  type === 0 ? new THREE.MeshPhongMaterial( { color: 0x00ff00 } ) :
-    					type === 1 ? new THREE.MeshPhongMaterial( { color: 0xff0000 } ) :
-    							 	 new THREE.MeshPhongMaterial( { color: 0x0000ff } ) ;
+    	let material =  type === 0 ? new THREE.MeshPhongMaterial( { 
+    									color: 0x00ff00,
+    									emissive: 0x00ff00,
+    									transparent: true,
+    									opacity: opacity 
+    								} ) :
+    					type === 1 ? new THREE.MeshPhongMaterial( { 
+    									color: 0xff0000,
+    									emissive: 0xff0000,
+    									transparent: true,
+    									opacity: opacity 
+    								} ) :
+    							 	 new THREE.MeshPhongMaterial( { 
+    							 	 	color: 0x0000ff,
+    							 	 	emissive: 0x0000ff,
+    							 	 	transparent: true,
+    							 	 	opacity: opacity 
+    							 	 } ) ;
 
     	pos 	 = pos 		|| new Vector3;
     	size 	 = size  	|| new Vector3;
--- app/assets/javascripts/scene.js
+++ app/assets/javascripts/scene.js
@@ -1,5 +1,6 @@
 let THREE = require('n3d-threejs');
 let TWEEN = require('tween');
+let Stats = require('stats-js');
 
 let Bar = require('./Bar');
 
@@ -8,18 +9,35 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 
 let scene, 
+	stats,
 	camera, 
 	renderer,
 	light,
 	barsContainer;
 
-let bars;
+let bars = {
+	t0: [],
+	t1: [],
+	t2: []
+};
 
-const nbBars = 512; 
+let initDate;
+
+const nbBars = 256; 
  
 function init () {
 
-	scene = new THREE.Scene()
+	initDate = Date.now();
+
+	stats = new Stats();
+
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.left = '0px';
+	stats.domElement.style.top = '0px';
+	 
+	document.body.appendChild( stats.domElement );
+
+	scene = new THREE.Scene();
 	
 	camera = new THREE.PerspectiveCamera( 45, width/height, 0.1, 10000 );
 	camera.position.z = 5;
@@ -28,7 +46,9 @@ function init () {
 	light.position.set( 0, 0, 2 );
 	scene.add( light );
 	
-	renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({
+		antialias: true
+	});
 
 	renderer.setSize( width, height );
 
@@ -54,6 +74,27 @@ function animate () {
  
 function update () {
 
+	stats.update();
+
+	let time = Date.now() - initDate;
+
+	let sin1 = Math.sin(time * 0.001) * 0.1;
+	let sin2 = Math.sin(time * 0.001 + 2 * Math.PI / 3) * 0.1;
+	let sin3 = Math.sin(time * 0.001 + 4 * Math.PI / 3) * 0.1;
+
+	bars.t0.forEach(bar => {
+		bar.mesh.position.y = sin3 * 0.1;
+		bar.mesh.position.z = sin1;
+	})
+	bars.t1.forEach(bar => {
+		bar.mesh.position.y = sin1 * 0.1;
+		bar.mesh.position.z = sin2;
+	})
+	bars.t2.forEach(bar => {
+		bar.mesh.position.y = sin2 * 0.1;
+		bar.mesh.position.z = sin3;
+	})
+
 }
  
 function render () {
@@ -64,23 +105,29 @@ function render () {
 
 function generateBars () {
 	
-	let randWidth, randHeight, randX, size, pos, bar;
+	let randWidth, randHeight, randType, randX, randOpacity, size, pos, bar;
 
 
 	for ( let i = 0; i < nbBars; i++ ) {
 
-		randWidth =  Math.random() / 50;
+		randWidth =  Math.random() / 30;
 		randHeight =  Math.random() * 5;
 		randX = (randomInt(0, 560) - 280 ) / 100;
+		randType = randomInt(0, 2);
+		randOpacity = randomInt(7, 10) * 0.1;
 
 		size = new THREE.Vector3( randWidth, randHeight, randWidth * 10 );
 		pos = new THREE.Vector3( randX, 0, 0 );
 
-		bar = new Bar( size, pos, randomInt(0, 3) );
+		bar = new Bar( size, pos, randType, randOpacity );
+
+		bars['t' + randType].push(bar);
 		
 		scene.add(bar.mesh);
 
 	}
+
+	console.log(bars);
 
 
 
