3f0f2c555d13b05e01fa152db73f78e3b57ec361
Julien Martins Da Costa <contact@julien-dacosta.fr>
2015-04-03 13:18:04 +0200

    Cordova

---
+++ cordova/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/AndroidManifest.xml \
---
+++ cordova/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes.dex : \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes/org/emaproject/hello/BuildConfig.class \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes/org/emaproject/hello/CordovaApp.class \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes/org/emaproject/hello/R$attr.class \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes/org/emaproject/hello/R$drawable.class \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes/org/emaproject/hello/R$string.class \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes/org/emaproject/hello/R$xml.class \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/classes/org/emaproject/hello/R.class \
+/Users/julien/Documents/gobelins/mise_en_application/emaproject-mobile/cordova/platforms/android/ant-build/dexedLibs/classes-613e6929d54ad0a5895eeade30daee97.jar \
